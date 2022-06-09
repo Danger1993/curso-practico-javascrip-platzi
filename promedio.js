4bdae3eef032bcaf8f1462d978950018be96c64c
Danger Gonzalez <dgonzalez@celcom.cl>
2022-06-09 11:04:07 -0400

    funcion mediana

--- promedio.js
+++ promedio.js
@@ -1,10 +1,4 @@
 function calcularMediaAritmetica(lista){
-    //let sumaLista = 0;
-
-    // for(let i=0; i < lista.length; i++){
-    //     sumaLista = sumaLista + lista[i];
-    // }
-
     const sumaLista = lista.reduce(
         function (valorAcumulado = 0, nuevoElemento){
             return valorAcumulado + nuevoElemento;
@@ -13,4 +7,20 @@ function calcularMediaAritmetica(lista){
 
     const promedioLista = sumaLista / lista.length;
     return promedioLista;
+}
+
+function calcularMediana(lista){
+
+lista.sort(function(a, b){return a - b}); 
+console.log('orden ' + lista);
+    if((lista.length % 2) === 0){
+        const elem1 = lista[parseInt(lista.length / 2) - 1];
+        const elem2 = lista[parseInt(lista.length / 2)];
+        const medianaLista = calcularMediaAritmetica([elem1, elem2]);
+        return medianaLista;
+    }else{
+        const medianaLista = lista[parseInt(lista.length / 2)];
+        return medianaLista;
+    }
+
 }
