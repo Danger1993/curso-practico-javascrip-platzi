f198559c4447dac1273973bd59d2168b20ea3def
Danger Gonzalez <dgonzalez@celcom.cl>
2022-06-09 14:36:54 -0400

    funcion moda

--- promedio.js
+++ promedio.js
@@ -24,3 +24,31 @@ console.log('orden ' + lista);
     }
 
 }
+
+function calcularModa(lista){
+    const lista1Count = {};
+    lista.map(
+        function (elemento){
+            if(lista1Count[elemento]){
+                lista1Count[elemento] += 1;
+            } else{
+                lista1Count[elemento] = 1;
+            }
+        }
+    );
+    const lista1Array = Object.entries(lista1Count).sort(
+        function (valorAcumulado, nuevoValor){
+           return  valorAcumulado[1] - nuevoValor[1];
+        }
+    );
+
+    const moda = lista1Array[lista1Array.length-1];
+    return moda;
+}
+
+function mode(arr){
+    return arr.sort((a,b) =>
+          arr.filter(v => v===a).length
+        - arr.filter(v => v===b).length
+    ).pop();
+}
