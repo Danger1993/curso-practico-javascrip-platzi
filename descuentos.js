41c18f325614d44cf4799aa4160faf8e2edcc336
Danger Gonzalez <dgonzalez@celcom.cl>
2022-06-08 23:54:45 -0400

    fin del taller 2

--- descuentos.js
+++ descuentos.js
@@ -1,6 +1,11 @@
 const preciOriginal = 150;
 const descuento = 19;
 
+const coupons = [
+    "JuanDC_es_Batman",
+    "pero_no_le_digas_a_nadie",
+    "es_un_secreto",
+];
 
 function calcularPrecioDescuento(precio, descuento){
     const porcentagePrecioConDescuento = 100 - descuento;
@@ -15,9 +20,24 @@ function precioReal(){
     const inputPrecio = document.getElementById("inputPrecio");
     const precio = inputPrecio.value;
 
-    const inputDescuento = document.getElementById("inputDescuento");
-    const descuento = inputDescuento.value;
+    const inputCoupon = document.getElementById("InputCoupon");
+    const couponValue = inputCoupon.value;
+
+    let descuento;
+
+    switch(couponValue) {
+        case coupons[0]: // "JuanDC_es_Batman"
+          descuento = 15;
+        break;
+        case coupons[1]: // "pero_no_le_digas_a_nadie"
+          descuento = 30;
+        break;
+        case coupons[2]: // "es_un_secreto"
+          descuento = 25;
+        break;
+      }
 
     const precioApagar = calcularPrecioDescuento(precio, descuento);
-    alert(precioApagar);
+     const resultP = document.getElementById("resultP");
+     resultP.innerText = "El precio con descuento es: $" + precioApagar;
 }
